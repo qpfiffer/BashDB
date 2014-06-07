6dc26f3fb8e824ff7ff268af00b36cf87e6e736a
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-06-06 18:49:10 -0700

    Fuck it, keys just can't have spaces in them.

--- src/set.sh
+++ src/set.sh
@@ -7,4 +7,11 @@ function bashdb_set {
         echo "You need a value to set."
         exit 1
     fi;
+
+    echo "Key: $2"
+    KEYHASH=$(echo $2 | sha1sum | awk '{ print $1 }')
+    # Have meta file that is key/value with offset into values file
+    # Hash key, use that to find the meta information
+    # Traverse bash array until key is the same, then use that offset into the values file
+    calc_idx $KEYHASH
 }
--- src/utils.sh
+++ src/utils.sh
@@ -1,12 +1,40 @@
-function get_table_size {
-    DBSIZE=$(stat $TABLENAME | grep 'Size' | awk '{ print $2 }')
+function get_values_file {
+    VALUESFILE=$TABLENAME/$TABLENAME$TABLEEXT
+}
+function get_meta_file {
+    METAFILE=$TABLENAME/$TABLENAME$METAEXT
+}
+function get_values_file_size {
+    get_values_file
+    DBSIZE=$(stat $VALUESFILE | grep 'Size' | awk '{ print $2 }')
+}
+function get_meta_file_size {
+    get_meta_file
+    DBSIZE=$(stat $METAFILE | grep 'Size' | awk '{ print $2 }')
 }
 function bashdb_meta {
     get_table_size
     echo "Database is $DBSIZE bytes long."
 }
 function create_tables {
+    get_values_file
+    get_meta_file
+
     mkdir $TABLENAME
-    truncate -s $HASHBLOCK'K' $TABLENAME/$TABLENAME'.0'$TABLEEXT
-    get_table_size
+
+    truncate -s $HASHBLOCK $VALUESFILE
+    truncate -s $METABLOCK $METAFILE
+
+    get_values_size
+}
+function create_required_partitions {
+    REQUIRED_EXTRA_SIZE=$1
+}
+function calc_idx {
+    HASH=$1
+
+    get_meta_file_size
+    ((TABLESIZE_MINUS_ONE = $DBSIZE - 1))
+    ((IDX = 16#$HASH & $TABLESIZE_MINUS_ONE))
+    echo "Index: $IDX"
 }
