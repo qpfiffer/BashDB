128de5304522c807614c871f19a28b61272d1d69
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-06-06 17:00:14 -0700

    More usage. Readme. Gitignore.

---
+++ README.md
@@ -0,0 +1,8 @@
+Installation
+============
+
+```bash
+make
+````
+
+Running the command without any arguments will print usage.
--- src/usage.sh
+++ src/usage.sh
@@ -4,5 +4,9 @@ $0 $VERSION
 
 usage: $0 [OPTIONS] [operation [arg [arg ...]]]
     -d <directory>  The location of the database to perform operations on.
+                    Defaults to the current directory.
+Operations:
+    set <key> <value>   Sets the value for <key>.
+    get <key>           Returns the value for <key>.
 EOF
 }
